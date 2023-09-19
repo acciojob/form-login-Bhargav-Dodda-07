4bbdc169a590c8a23c3f97a0cd06d8d8015f7345
DODDA BHARGAV <128250258+Bhargav-Dodda-07@users.noreply.github.com>
2023-09-19 21:11:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
 
+	var form = document.getElementById("form1")
+
+	var firstName = form.elements["firstName"].value;
+  var lastName = form.elements["lastName"].value;
+	
+	alert("First Name: " + firstName + "\nLast Name: " + lastName);
+
 }
+
+document.getElementById("form1").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the form from submitting
+  getFormValue(); // Call the getFormValue function
+});
